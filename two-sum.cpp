b976f6919b1ca10ab5ec77f9195c7a8069c49ac2
Faisal-Hawsawi <qqqfaisal379@gmail.com>
2024-09-16 22:01:36 +1200

    done with two sums

--- two-sum.cpp
+++ two-sum.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
 int partition(vector<int>& arr, int low, int high){
@@ -37,7 +38,27 @@ void quick_sort(vector<int>& num_list, int low, int high){
 vector<int> twoSum(vector<int>& nums, int target) {
 
     vector<int> ans;
+    unordered_map<int, vector<int>> num_to_pos;
     int size = nums.size();
+
+    for(int i = 0; i < size; i ++){
+        num_to_pos[nums.at(i)].push_back(i);
+    }
+
+    // if (num_to_pos.find(7) != num_to_pos.end()){
+    //     for(int pos : num_to_pos[7]){
+    //         cout << "ello? " << pos << " ";
+    //     }
+    //     cout << endl;
+    // }
+    //
+    // else{
+    //     cout << "not there" << endl;
+    // }
+
+    // for (auto it : num_to_pos)
+    //     cout << "here ello?: " << it.first << " " << it.second.front() << endl; 
+    
     quick_sort(nums, 0, size - 1);
 
     // for (int i = 0; i < nums.size(); i++){ 
@@ -47,11 +68,20 @@ vector<int> twoSum(vector<int>& nums, int target) {
     int end = nums.size()-1;
     int start = 0;
 
+
     while (end > start){
-        int is_equal_to_target = nums.at(start) + nums.at(end);
+        int add1 = nums.at(start) , add2 = nums.at(end);
+        int is_equal_to_target = add1 + add2;
         if (is_equal_to_target == target){
-            ans.push_back((start));
-            ans.push_back((end));
+            for (int pos : num_to_pos[add1] ){
+                ans.push_back(pos);
+            }
+
+            if (add1 != add2){
+                for (int pos : num_to_pos[add2] ){
+                    ans.push_back(pos);
+                }
+            }
             end--;
         }
         else if (is_equal_to_target > target){
@@ -62,15 +92,22 @@ vector<int> twoSum(vector<int>& nums, int target) {
         }
     }
 
+    cout << "your answer: ";
     for (int i = 0; i < ans.size(); i++){ 
-        cout << ans.at(i) << endl;
+        cout << ans.at(i) << " ";
     }
+    cout << endl;
     
     return ans;
 }
 
 int main(void){
     
-    vector<int> arr = {2,7,11,15};
+    vector<int> arr = {2,7,11,15}; // [0,1]
+    vector<int> arr2 = {3,2,4}; // [1,2]
+    vector<int> arr3 = {3,3}; // [0,1]
+    
     twoSum (arr, 9);
+    twoSum (arr2, 6);
+    twoSum (arr3, 6);
 }
